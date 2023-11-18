6d1fbc341a0ebafa6c5ec4e2340f8b343f5b37b4
Igor Silva Pivoto <136984668+IgorPivoto@users.noreply.github.com>
2023-11-18 08:30:51 -0300

    atualização

--- canvas2.js
+++ canvas2.js
@@ -8,19 +8,17 @@ canvas.height = window.innerHeight;
 
 let inicioX = 0;
 let inicioY = 200;
-let timer = 0;
+
 
 function GeraCirculo() {   
-    timer++;
 
 
-    if (timer <= 1) {
+
         inicioX += 2.3;  
         inicioY += 0.3;  
-        timer = 0; 
-    }
+        
     
-    objetos.clearRect(0, 0, canvas.width, canvas.height);  
+
 
     objetos.beginPath();
     objetos.arc(inicioX, inicioY, 50, 0, Math.PI * 2, false);
